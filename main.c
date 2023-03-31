f96a6098cd4d04863c1887e0d2173dfc1ba16938
Melina <mrequena@fue.undef.edu.ar>
2023-03-31 10:39:22 -0300

    Parcialito

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
-#include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+//Desarrollar un programa que pida al usuario, por pantalla, que ingrese las notas de los alumnos del curso.
+// Las notas se seguirán cargando hasta que ingrese -1.
+// Una vez que finalice la carga, se tiene que mostrar por pantalla el promedio.
+
+#include<stdio.h>
+int main()
+{
+    float suma, nota, prom;
+    int cont=0;
+  do{
+      printf("Ingrese nota del alumno, presione -1 para finalizar\n");
+      scanf("%f", &nota);
+      cont++;
+      suma=suma+nota;
+  }while(nota!=-1);
+  prom=suma/(cont-1);
+    printf("El promedio del alumno es %f",prom);
     return 0;
+
 }
